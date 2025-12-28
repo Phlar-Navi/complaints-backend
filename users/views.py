f14c20da518f150ad5b3b242600706120a3bb389
Raphael <smrcorp007@gmail.com>
2025-12-28 09:57:14 +0100

    Encore et Toujours  une Tentative de correction de la logique de login multi-tenant cassée en pré-production

--- users/views.py
+++ users/views.py
@@ -25,6 +25,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import authenticate
 
 from rest_framework.exceptions import ValidationError
 from django.db import connection
